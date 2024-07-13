6120ee9bd75f92a22c7041d839a5c0cd19f06a9a
pmahanty1983 <pmahanty1983@gmail.com>
2024-07-13 14:48:43 +0530

    The code to check the recepients balance basis pubKey.

---
+++ generate-keypair/check-balance.ts
@@ -0,0 +1,13 @@
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+
+const publicKey = new PublicKey("Bo8vBSqo9GLywqPo1wgZheRvKc3nwipeMMn6SEtfuYgS");
+
+const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+  `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+);
